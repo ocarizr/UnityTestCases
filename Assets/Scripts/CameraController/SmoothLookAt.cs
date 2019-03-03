97a5038884ecdedcdc6ff1935c673a66d925c458
ocarizr <rafael.ocariz90@gmail.com>
2019-03-02 22:00:28 -0300

    First commit, tested the first desired feature

---
+++ Assets/Scripts/CameraController/SmoothLookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SmoothLookAt : MonoBehaviour
+{
+    private const float MIN_SPEED = 2f;
+    private const float MAX_SPEED = 20f;
+
+    public Transform Player;
+
+    [SerializeField] [Range(MIN_SPEED, MAX_SPEED)] private float _smoothSpeed;
+    private Quaternion _lookRotation;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!Player) return;
+
+        _lookRotation = Quaternion.LookRotation(Player.position - transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, _smoothSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    public float SpeedH = 2.0f;
+    public float SpeedV = 2.0f;
+
+    private float _yaw = 0.0f;
+    private float _pitch = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * MoveSpeed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector3.up * MoveSpeed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(Vector3.down * MoveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        _yaw += SpeedH * Input.GetAxis("Mouse X");
+        _pitch -= SpeedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(_pitch, _yaw, 0.0f);
+    }
+}

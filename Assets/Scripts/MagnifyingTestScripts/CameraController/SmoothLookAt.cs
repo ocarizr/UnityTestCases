93143a1ce57a51d7435d6b5631b3ca4c9d2b8a3f
Rafael <rafael.ocariz90@gmail.com>
2019-03-22 12:09:41 -0300

    Update Project Structure

---
+++ Assets/Scripts/MagnifyingTestScripts/CameraController/SmoothLookAt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.MagnifyingTestScripts.CameraController
+{
+    public class SmoothLookAt : MonoBehaviour
+    {
+        private const float MinSpeed = 2f;
+        private const float MaxSpeed = 20f;
+
+        public Transform Player;
+
+        [SerializeField] [Range(MinSpeed, MaxSpeed)] private readonly float _smoothSpeed = MinSpeed;
+        private Quaternion _lookRotation;
+
+        // Update is called once per frame
+        private void LateUpdate()
+        {
+            if (!Player) return;
+
+            _lookRotation = Quaternion.LookRotation(Player.position - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, _smoothSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/TouchInput Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput Scripts/TouchInput.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Assets.Scripts
+namespace Assets.Scripts.TouchInput_Scripts
 {
     public class TouchInput : MonoBehaviour
     {
